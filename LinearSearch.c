aaef2fee77b7130cbf0811c1e3804cc7829f4ec1
Maks9m <maxim.kramarenko8a@gmail.com>
2024-10-18 01:40:00 +0300

    not finished

--- LinearSearch.c
+++ LinearSearch.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int n;
+void makeMatrix(float a[n][n]);
 
 int main(void)
 {
@@ -15,5 +16,49 @@ int main(void)
         {
         scanf("%f", &matrix[i][j]);
         }
+    }
+    //printf("Ваша матриця: \n");
+    //makeMatrix(matrix);
+    
+
+    float a = 0;//Перший додатній член побічної діагоналі
+    int ai, aj = 0;//Рядок, стовпець;
+    float b = 0;//Останній відʼємний член побічної діагоналі
+    int bi, bj = 0;//Рядок, стовпець;
+    for (int i = 0; i < n; i++)
+    {
+        if(matrix[n-i][i] > 0)
+        {
+            if(a == 0)
+            {
+                a = matrix[n-i][i];
+                ai = n - i;
+                aj = i;
+            }
+        }
+        if(matrix[n-i][i] < 0)
+        {
+            b = matrix[n-i][i];
+            bi = n - i;
+            bj = i;
+        }
+    }
+    
+    matrix[ai][aj] = b;
+    matrix[bi][bj] = a;
+    printf("Змінена матриця: \n");
+    makeMatrix(matrix);
+
+}
+
+void makeMatrix(float a[n][n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%f ", a[i][j]);
+        }
+        printf("\n");
     }
 }
