173324676b66bac5add379e6326d18dc7c1fbc4e
Maks9m <maxim.kramarenko8a@gmail.com>
2024-10-17 23:01:51 +0300

    first commit

---
+++ LinearSearch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int n;
+
+int main(void)
+{
+    printf("Введіть розмірність квадратної матриці: \n");
+    scanf("%i", &n);
+    float matrix[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Введіть %i рядок\n", i+1 );
+        for (int j = 0; j < n; j++)
+        {
+        scanf("%f ", &matrix[i][j]);
+        }
+    }
+}
